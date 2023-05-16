9fc3e8ab5a73ed98d43db21e5aec8be888220b1a
Guilherme Balog Gardino <guibalog@gmail.com>
2023-05-16 19:06:10 -0300

    add dom ready listener in main script

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,24 @@
-const jobTitleVariants = [
-  'de Software',
-  'Web',
-  'Front-end',
-  'Back-end',
-];
+document.addEventListener('DOMContentLoaded', () => {
+  const jobTitleVariants = [
+    'de Software',
+    'Web',
+    'Front-end',
+    'Back-end',
+  ];
 
-const variantsEl = document.querySelector('.hero-section__job-title-variant');
+  const variantsEl = document.querySelector('.hero-section__job-title-variant');
+  animateJobTitle(jobTitleVariants, variantsEl, 1500);
 
-/**
- * @param {string[]} variants
- * @param {HTMLElement} el
- * @param {number} timing
- */
-function animateJobTitle(variants, el, timing) {
-  let i = 0;
-  setInterval(() => {
-    i = (i + 1) % variants.length;
-    el.innerHTML = variants[i];
-  }, timing);
-}
-
-animateJobTitle(jobTitleVariants, variantsEl, 1500);
+  /**
+  * @param {string[]} variants
+  * @param {HTMLElement} el
+  * @param {number} timing
+  */
+  function animateJobTitle(variants, el, timing) {
+    let i = 0;
+    setInterval(() => {
+      i = (i + 1) % variants.length;
+      el.innerHTML = variants[i];
+    }, timing);
+  }
+});
