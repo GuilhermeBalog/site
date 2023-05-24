e8025c64d0a014bd392d8c9451dcc8f6e0b611f4
Guilherme Balog Gardino <guibalog@gmail.com>
2023-05-23 22:07:05 -0300

    refactor: extract addTime to other file

---
+++ src/lib/addTime.js
@@ -0,0 +1,46 @@
+/**
+ * @param {Object} experience
+ * @param {Date} experience.start
+ * @param {Date} [experience.end]
+ */
+export function addTime(experience) {
+  return {
+    ...experience,
+    time: createTime(experience.start, experience.end),
+  };
+}
+
+/**
+ * @param {Date} start
+ * @param {Date} [end]
+ */
+function createTime(start, end) {
+  const from = formatDate(start);
+  const to = end ? formatDate(end) : 'Atualmente';
+
+  return `${from} - ${to}`;
+}
+
+const dateFormatter = new Intl.DateTimeFormat('pt-BR', {
+  month: 'short',
+  year: 'numeric'
+});
+/** @param {Date} date */
+function formatDate(date) {
+  const formatted = dateFormatter.format(date);
+  const capitalized = capitalize(formatted);
+  const withoutDe = removeDe(capitalized);
+
+  return withoutDe;
+}
+
+/** @param {string} text */
+function capitalize(text) {
+  const [firstLetter, ...rest] = text.split('');
+  return firstLetter.toUpperCase().concat(rest.join(''));
+}
+
+/** @param {string} text */
+function removeDe(text) {
+  return text.replace('de ', '');
+}
--- src/lib/getEducation.js
+++ src/lib/getEducation.js
@@ -1,47 +1,9 @@
 import { readJson } from './readJson.js';
 import { EDUCATION_PATH } from './paths.js';
+import { addTime } from './addTime.js';
 
 export async function getEducation() {
   const education = await  readJson(EDUCATION_PATH);
 
-  return education.map(e => ({
-    ...e,
-    time: createTime(e.start, e.end)
-  }));
-}
-
-/**
- * @param {Date} start
- * @param {Date} [end]
- */
-function createTime(start, end) {
-  const from = formatDate(start)
-  const to = end ? formatDate(end) : 'Atualmente';
-
-  return `${from} - ${to}`
-}
-
-const dateFormatter = new Intl.DateTimeFormat('pt-BR', {
-  month: 'short',
-  year: 'numeric'
-});
-
-/** @param {Date} date */
-function formatDate(date) {
-  const formatted = dateFormatter.format(date);
-  const capitalized = capitalize(formatted);
-  const withoutDe = removeDe(capitalized);
-
-  return withoutDe;
-}
-
-/** @param {string} text */
-function capitalize(text) {
-  const [firstLetter, ...rest] = text.split('');
-  return firstLetter.toUpperCase().concat(rest.join(''))
-}
-
-/** @param {string} text */
-function removeDe(text) {
-  return text.replace('de ', '')
+  return education.map(e => addTime(e));
 }
